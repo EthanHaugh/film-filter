14f7525bf19fc575c181ac54dc763454768d18fc
Ethan Haugh <ethanhaugh201@gmail.com>
2026-05-31 23:28:39 +1000

    Remove Build Files

--- presets/kodak.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipelineConfig } from '../src/pipeline';
-/**
- * Kodak Portra - Warm, vintage-looking color film stock
- * Known for: Warm tones, smooth gradations, reduced saturation, nostalgic feel
- */
-export declare const kodak: PipelineConfig;
-//# sourceMappingURL=kodak.d.ts.map
